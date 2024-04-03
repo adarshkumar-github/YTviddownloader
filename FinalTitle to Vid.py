aa876f702a021c1884f9ef4a5159e38a37419ee0
adarshkumar-github <146330090+adarshkumar-github@users.noreply.github.com>
2024-04-03 20:21:37 +0530

    Update FinalTitle to Vid.py

--- FinalTitle to Vid.py
+++ FinalTitle to Vid.py
@@ -12,6 +12,7 @@ search_query = '+'.join(video_title.split())
 url = f"https://www.youtube.com/results?search_query={search_query}"
 
 driver.get(url)
+print("Working on it......")
 
 driver.implicitly_wait(10)
 
@@ -33,17 +34,34 @@ url = YouTube(link)
 save_location = r"C:\Users\adars\OneDrive\Documents\Experiment"
 
 # showing video details
+print()
 print("Video title: ", url.title)
 print("Number of views on the video: ",url.views)
 print("Length of the video ", url.length, "seconds")
 print("Description:", url.description)
+print()
 
-try:
-    url = YouTube(link)
-    video = url.streams.get_highest_resolution()
-    print("Downloading.............")
-    video.download(save_location)
-    print("Video downloaded successfully!!!")
+print("*-----------------*")
+print()
+print("Choose the Format in which you want to download the file: ")
+print("Audio(press a)")
+print("Video(press v)")
+answer=input()
+url = YouTube(link)
+video = url.streams.get_highest_resolution()
+audio =url.streams.get_audio_only()
 
-except:
-    print("Something went wrong!!")
+if (answer=='a'):
+    try:
+        print("Downloading.............")
+        audio.download(save_location)
+        print("Audio downloaded successfully!!!")
+    except:
+        print("Something went wrong!!")
+elif (answer=='v'):
+    try:
+        print("Downloading.............")
+        video.download(save_location)
+        print("Video downloaded successfully!!!")
+    except:
+        print("Something went wrong!!")
